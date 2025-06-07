a7b327f52a055b79833dd1b119c9306706614b43
DpkPatil05 <patildeepak188@gmail.com>
2025-06-07 18:47:38 +0530

    Added data table.

--- spectrum-view/src/store/index.ts
+++ spectrum-view/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import addSerialNumberReducer from "../features/add-serial-number/addSerialNumberSlice";
 import getStockInformationReducer from "../features/get-stock-information/getStockInformationSlice";
+import getMobileUsersSalesAndCommissionReducer from "../features/get-mobile-users-sales-and-commission/getMobileUsersSalesAndCommissionSlice";
 
 export const store = configureStore({
   reducer: {
     addSerialNumber: addSerialNumberReducer,
     getStockInformation: getStockInformationReducer,
+    getMobileUsersSalesAndCommission: getMobileUsersSalesAndCommissionReducer,
   },
 });
 
