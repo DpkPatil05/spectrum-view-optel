a00f416ed5ecaf8d37acc8374d496b5b297e6636
DpkPatil05 <patildeepak188@gmail.com>
2025-06-08 22:27:54 +0530

    Added QR code scanner.

--- spectrum_painter/lib/feature/verify_product/pages/verify_product_screen_widget.dart
+++ spectrum_painter/lib/feature/verify_product/pages/verify_product_screen_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../bloc/verify_product_bloc.dart';
 
@@ -13,6 +14,35 @@ class VerifyProductScreenWidget extends StatefulWidget {
 
 class _VerifyProductScreenWidgetState extends State<VerifyProductScreenWidget> {
   late final VerifyProductBloc bloc;
+  final MobileScannerController controller = MobileScannerController(
+    torchEnabled: true,
+    autoZoom: true,
+  );
+  Barcode? _barcode;
+
+  Widget _barcodePreview(Barcode? value) {
+    if (value == null) {
+      return const Text(
+        'Scan something!',
+        overflow: TextOverflow.fade,
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Text(
+      value.displayValue ?? 'No display value.',
+      overflow: TextOverflow.fade,
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -22,6 +52,28 @@ class _VerifyProductScreenWidgetState extends State<VerifyProductScreenWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Verify Product'));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Paint serial number qr scanner')),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          MobileScanner(onDetect: _handleBarcode),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 100,
+              color: const Color.fromRGBO(0, 0, 0, 0.4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Center(child: _barcodePreview(_barcode))),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
